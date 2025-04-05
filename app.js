dc02afce9c9837dfd32eae18d9050ca1b4d23b8b
Luke Beno <lgbeno@tundra-labs.com>
2025-04-05 06:59:59 -0500

    Revert "Attempt to fix bug where connecting message disappears"
    This reverts commit 0be1e962ae0962b275de8d2c970ea32c09e79e49.

--- app.js
+++ app.js
@@ -89,9 +89,7 @@
         window.addEventListener('connectedChange', async function(event) {
             const isConnected = event.detail;
             if (isConnected && window.getComputedStyle(popupBox).display === 'none') {
-                if (!calibration_values.fw_id.match("BTL*")) { // make sure this element stays visible if in Bootloader state of connection
-                    document.getElementById('connectMessage').style.display = 'none'; // todo: remove midi error field?
-                }
+                document.getElementById('connectMessage').style.display = 'none'; // todo: remove midi error field?
                 //console.log(calibration_values.fw_id.value);
                 //console.log(calibration_values.voltage.value);
 
